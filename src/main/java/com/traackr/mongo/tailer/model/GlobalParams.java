2403a49ea4293a85dc3f02b4285f218483c5089a
winder <wwinder.unh@gmail.com>
2017-07-20 11:58:32 -0400

    Apply MIT license to project.

--- src/main/java/com/traackr/mongo/tailer/model/GlobalParams.java
+++ src/main/java/com/traackr/mongo/tailer/model/GlobalParams.java
@@ -1,16 +1,25 @@
 /**
- * GlobalParams.java - Traackr, Inc.
- *
- * This document set is the property of Traackr, Inc., a Massachusetts
- * Corporation, and contains confidential and trade secret information. It
- * cannot be transferred from the custody or control of Traackr except as
- * authorized in writing by an officer of Traackr. Neither this item nor the
- * information it contains can be used, transferred, reproduced, published,
- * or disclosed, in whole or in part, directly or indirectly, except as
- * expressly authorized by an officer of Traackr, pursuant to written
- * agreement.
- *
- * Copyright 2012-2015 Traackr, Inc. All Rights Reserved.
+ * MIT License
+ *  
+ * Copyright (c) 2017 Traackr, Inc
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *   SOFTWARE.
  */
 package com.traackr.mongo.tailer.model;
 
